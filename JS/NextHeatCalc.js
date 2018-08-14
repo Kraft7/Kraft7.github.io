c3cc08eaa19b255a4fa5fca0353fcd78c3b64dd8
Holly <hollycatsmith@gmail.com>
2018-08-14 20:58:18 +1200

    .

--- JS/NextHeatCalc.js
+++ JS/NextHeatCalc.js
@@ -1,6 +1,4 @@
 function calculateHeat() {
-  document.getElementById("hiddenText").innerHTML = "";
-
   year = document.getElementById("year").value;
   month = document.getElementById("month").value;
   cooldown = document.getElementById("cooldown").value;
@@ -8,15 +6,13 @@ function calculateHeat() {
   if (cooldown == "" || month == "" || year == "") {
     document.getElementById("hiddenText").innerHTML = "Please enter all the feilds.";
   }
-  else if (cooldowns > 20 || cooldown < 0) {
+  else if (cooldown > 20 || cooldown < 0) {
     document.getElementById("hiddenText").innerHTML = "Cooldown must be between 0 and 20.";
   }
-
-  else if (months > 12 || months < 0) {
+  else if (month > 12 || month < 0) {
     document.getElementById("hiddenText").innerHTML = "Months must be between 0 and 12.";
   }
-
-  else if (years > 16 || years < 2) {
+  else if (year > 16 || year < 2) {
     document.getElementById("hiddenText").innerHTML = "Years must be between 2 and 16.";
   }
   else if (year == 14 && month > 1) {
