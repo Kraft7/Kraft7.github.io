74d69083517f90bd2c1f295b862a1bd299d55eee
Holly <hollycatsmith@gmail.com>
2018-08-14 20:51:55 +1200

    .

--- JS/HeatsLeftCalc.js
+++ JS/HeatsLeftCalc.js
@@ -8,6 +8,17 @@ function lifetimeCalc() {
   if (cooldowns == "" || months == "" || years == "") {
     document.getElementById("hiddenText2").innerHTML = "Please enter all the feilds.";
   }
+  else if (cooldowns > 20 || cooldown < 0) {
+    document.getElementById("hiddenText2").innerHTML = "Cooldown must be between 0 and 20.";
+  }
+
+  else if (months > 12 || months < 0) {
+    document.getElementById("hiddenText2").innerHTML = "Months must be between 0 and 12.";
+  }
+
+  else if (years > 16 || years < 2) {
+    document.getElementById("hiddenText2").innerHTML = "Years must be between 2 and 16.";
+  }
   else if (years == 14 && months > 1) {
     document.getElementById("hiddenText2").innerHTML = "Lioness cannot breed past 14 years!";
   }
--- JS/NextHeatCalc.js
+++ JS/NextHeatCalc.js
@@ -8,7 +8,17 @@ function calculateHeat() {
   if (cooldown == "" || month == "" || year == "") {
     document.getElementById("hiddenText").innerHTML = "Please enter all the feilds.";
   }
+  else if (cooldowns > 20 || cooldown < 0) {
+    document.getElementById("hiddenText").innerHTML = "Cooldown must be between 0 and 20.";
+  }
 
+  else if (months > 12 || months < 0) {
+    document.getElementById("hiddenText").innerHTML = "Months must be between 0 and 12.";
+  }
+
+  else if (years > 16 || years < 2) {
+    document.getElementById("hiddenText").innerHTML = "Years must be between 2 and 16.";
+  }
   else if (year == 14 && month > 1) {
     document.getElementById("hiddenText").innerHTML = "Lioness cannot breed past 14 years!";
   }
